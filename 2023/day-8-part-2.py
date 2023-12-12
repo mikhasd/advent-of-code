6901bf2602cc9390a0e1060afc2a0cc89b484570
Mikhail Domanoski <332960+mikhasd@users.noreply.github.com>
2023-12-12 20:54:47 +0000

    Day 8 Part 2

--- 2023/day-8-part-2.py
+++ 2023/day-8-part-2.py
@@ -1,6 +1,7 @@
 import re
 from typing import Generator, Tuple, Dict, Iterable
 from helper import load_input_lines
+import math
 
 INSTRUCTION = re.compile(r'^(?P<source>\w{3})\s+=\s+\((?P<left>\w{3}),\s+(?P<right>\w{3})')
 
@@ -12,22 +13,17 @@ def add_map_node(map: Dict[str, Tuple[str, str]], line: str):
     right = groups[2]
     map[source] = (left, right)
 
-def count_steps(map: Dict[str, Tuple[str, str]], start_points: Iterable[str], instructions: str) -> int:
+def count_steps(map: Dict[str, Tuple[str, str]], start_point: str, instructions: str) -> int:
     encoded_instructions = [0 if instruction == 'L' else 1 for instruction in instructions]
-    matches = len(start_points)
-    nodes = [map[start_point] for start_point in start_points]
+    node = map[start_point]
     steps = 0
     while True:
         for instruction in encoded_instructions:
             steps += 1
-            zs = 0
-            for i, node in enumerate(nodes):
-                next_node = node[instruction]
-                nodes[i] = map[next_node]
-                if next_node[2] == 'Z':
-                    zs += 1
-                    if zs == matches:
-                        return steps
+            next_node = node[instruction]
+            node = map[next_node]
+            if next_node[2] == 'Z':
+                return steps
 
 def solve():
     lines = load_input_lines(8)
@@ -40,8 +36,9 @@ def solve():
     starting_points = list(filter(lambda p: p[2] == 'A', map.keys()))
     print(f'starting points: {starting_points}')
 
-    steps = count_steps(map, starting_points, instructions)
-    print(steps)
+
+    steps = [count_steps(map, starting_point, instructions) for starting_point in starting_points]
+    print(math.lcm(*steps))
 
 
     
