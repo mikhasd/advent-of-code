825a81fbbc7e40bc811bbd265da9d7a4613cf224
Mikhail Domanoski <332960+mikhasd@users.noreply.github.com>
2024-01-05 09:17:29 +0000

    update helpers

--- 2023/helper.py
+++ 2023/helper.py
@@ -4,20 +4,20 @@ from pathlib import Path
 aoc_2023 = Path(__file__).parent
 
 def load_input(day: int) -> str:
-    day_input_path = aoc_2023.joinpath(f'day-{day}.input.txt')
+    day_input_path = aoc_2023.joinpath(f'day-{day:02d}.input.txt')
     if not day_input_path.exists():
-        raise Exception(f'missing input file day-{day}.input.txt on directory {aoc_2023}')
+        raise Exception(f'missing input file day-{day:02d}.input.txt on directory {aoc_2023}')
     with day_input_path.open('r') as input_file:
         return input_file.read()
 
 def load_input_lines(day: int) -> Generator[int, None, None]:
-    day_input_path = aoc_2023.joinpath(f'day-{day}.input.txt')
+    day_input_path = aoc_2023.joinpath(f'day-{day:02d}.input.txt')
     if not day_input_path.exists():
-        raise Exception(f'missing input file day-{day}.input.txt on directory {aoc_2023}')
+        raise Exception(f'missing input file day-{day:02d}.input.txt on directory {aoc_2023}')
     with day_input_path.open('r') as input_file:
         line = input_file.readline()
         while line:
-            yield line
+            yield line[:-1] # Remove trailing \n - might break some challenges already developed
             line = input_file.readline()
 
 
--- 2023/template.py
+++ 2023/template.py
@@ -1,10 +1,11 @@
 #import re
 #from typing import Generator
-#from helper import load_input, load_input_lines
+from helper import load_input, load_input_lines
 
 
 def solve():
-    pass
+    for line in load_input_lines(0):
+        pass
     
 
 if __name__ == '__main__':
